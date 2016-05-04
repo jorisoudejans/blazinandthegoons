4008a451fbe3a2970e55bff36bd43acab53f0cd7
Shane Koppers <shanekoppers@hotmail.com>
2016-05-04 16:08:58 +0200

    Expanded action and preset model. Fixed tests.

--- app/models/Preset.java
+++ app/models/Preset.java
@@ -5,18 +5,21 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import play.data.validation.Constraints;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 /**
  * Created by Shane on 3-5-2016.
  */
+@Entity
 public class Preset extends Model {
 
     @Id
     public Long id;
 
     public String name;
+    public int camera;
     public float pan;
     public float tilt;
     public float zoom;
@@ -25,9 +28,10 @@ public class Preset extends Model {
     public static Finder<Long, Preset> find = new Finder<Long,Preset>(Preset.class);
 
 
-    public static Preset createPreset(String name, float pan, float tilt, float zoom, float focus) {
+    public static Preset createPreset(String name, int camera, float pan, float tilt, float zoom, float focus) {
         Preset pr = new Preset();
         pr.name = name;
+        pr.camera = camera;
         pr.pan = pan;
         pr.tilt = tilt;
         pr.zoom = zoom;
