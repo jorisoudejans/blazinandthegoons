97d788fe826ac2267938a26d45693f12b6e9d19b
Hidde Lycklama <hidde@vlinderstorm.com>
2016-05-11 14:30:10 +0200

    Make unit tests pass on command line, not only on unit-test.html Add jasmine typings for typescript compiler

--- app/assets/test/script.spec.ts
+++ app/assets/test/script.spec.ts
@@ -1,4 +1,8 @@
+/// <reference path="../../../typings/jasmine.d.ts"/>
+
 import { Script } from './app/api/script';
+import {describe, it, beforeEach, expect} from 'angular2/testing';
+
 describe('Script', () => {
     it('has name', () => {
         let hero: Script = {id: 1, name: 'Super Cat'};
