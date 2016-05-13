20c00022015fffbbc54cba5fd39bb9ef0be97925
Joris <joris.oudejans@gmail.com>
2016-05-13 11:23:06 +0200

    - Socket ready for requests

--- app/assets/app/app.component.ts
+++ app/assets/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HTTP_PROVIDERS }    from "angular2/http";
 import {OnInit, Component} from "angular2/core"
-import {$WebSocket}     from 'assets/lib/angular2-websocket/angular2-websocket'
 
 import {ScriptService} from "./api/script.service";
 import {Script, ActiveScript} from "./api/script";
@@ -8,7 +7,6 @@ import {ScriptListComponent} from "./scriptlist.component";
 import {ActionListComponent} from "./actionlist.component";
 import {PresetListComponent} from "./presetlist.component";
 import {CameraListComponent} from "./cameralist.component";
-
 
 @Component({
     selector: "script-dd",
@@ -21,29 +19,41 @@ import {CameraListComponent} from "./cameralist.component";
 })
 export class AppComponent implements OnInit {
     socket: WebSocket;
-    constructor (private _heroService: ScriptService) {}
+    constructor (private _scriptService: ScriptService) {}
     currentScript: ActiveScript;
     ngOnInit() {
-        this.connect()
+        this.connect();
+        this.socket.onmessage = function(ev) {
+            console.log(ev);
+        }
+    }
+    onMessage(ev: MessageEvent) {
+        console.log(ev);
+        this.currentScript = JSON.parse(ev.data);
     }
     connect() {
-        this.socket = this._heroService.connectScript();
-        this.socket.onmessage = function(ev){
+        this.socket = this._scriptService.connectScript();
+        /*let socket = this.socket;
+        this.socket.onopen = function(ev) {
+            console.log(ev);
+            socket.send("hoi");
+        };
+        this.socket.onmessage = function(ev) {
+            console.log(ev);
             console.log("Received data from websocket: ", ev.data);
         };
-        this.socket.send("hoi");
-        console.log(this.socket);
+        this.socket.onerror = function(ev) {
+            console.log("Error from websocket: ", ev.data);
+        };
+        console.log(this.socket);*/
     }
-    getStatus(id: number) {
-        this._heroService.getStatus(id)
+    /*getStatus(id: number) {
+        this._scriptService.getStatus(id)
             .subscribe(
                 currentScript => {this.currentScript = currentScript}
         );
-    }
+    }*/
     startScript(id: number) {
-        this._heroService.startScript(id)
-            .subscribe(
-                currentScript => {this.currentScript = currentScript}
-        );
+        ScriptService.startScript(this.currentScript, this.socket);
     }
 }
