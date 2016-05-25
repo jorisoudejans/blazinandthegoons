6c9896abfa1cadfce5da957ce3d709962e2dc274
Hidde Lycklama <mail.hidde@gmail.com>
2016-05-25 21:33:36 +0200

    Add tests for multiple sockets to ScriptSocketTest and add a synchronous race condition test Improve testability of ScriptSocket

--- test/util/socket/ScriptSocketTest.java
+++ test/util/socket/ScriptSocketTest.java
@@ -1,6 +1,8 @@
 package util.socket;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import models.ActiveScript;
+import models.Script;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,16 +11,17 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import play.Application;
+import play.libs.Json;
 import play.mvc.WebSocket;
 import play.test.Helpers;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.function.Consumer;
 
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static play.test.Helpers.fakeApplication;
 
 /**
@@ -71,13 +74,79 @@ public class ScriptSocketTest {
         verify(socket_out_p1).write(any(JsonNode.class)); // verify write was called
     }
 
-/*
-    public void test_outputBoth() {
+    /**
+     * Tests adding a single socket and writing to the socket with no active script.
+     */
+    @Test
+    public void test_singleWriteNoActiveScript() {
+        scriptSocket.join(socket_in_p1, socket_out_p1); // add the mocks
+        JsonNode node = mock(JsonNode.class);
+        scriptSocket.processInput(socket_in_p1, node);
+
+        verify(socket_out_p1, times(2)).write(Json.toJson(new ArrayList<>())); // retrieve empty array for no active script
+    }
+
+    /**
+     * Tests adding a single socket and writing to the socket with active script.
+     */
+    @Test
+    public void test_singleWrite() {
+        scriptSocket.join(socket_in_p1, socket_out_p1); // add the mocks
+        ActiveScript activeScript = activateScript();
+        activeScript.actionIndex++; // IMPORTANT: don't save!
+        scriptSocket.processInput(socket_in_p1, Json.toJson(activeScript));
+
+        verify(socket_out_p1, times(1)).write(Json.toJson(activeScript)); //
+    }
+
+    /**
+     * Tests adding two sockets and writing on one script with an activescript
+     */
+    @Test
+    public void test_doubleWrite() {
         scriptSocket.join(socket_in_p1, socket_out_p1);
         scriptSocket.join(socket_in_p2, socket_out_p2);
 
+        ActiveScript activeScript = activateScript();
+        activeScript.actionIndex++; // IMPORTANT: don't save!
+        scriptSocket.processInput(socket_in_p1, Json.toJson(activeScript));
 
-
+        verify(socket_out_p1, times(1)).write(Json.toJson(activeScript));
+        verify(socket_out_p2, times(1)).write(Json.toJson(activeScript));
     }
-*/
+
+    /**
+     * Tests adding two sockets and writing both sockets back forth
+     */
+    @Test
+    public void test_writeOneTwo_synchronous() {
+        scriptSocket.join(socket_in_p1, socket_out_p1);
+        scriptSocket.join(socket_in_p2, socket_out_p2);
+
+        ActiveScript activeScript = activateScript();
+        activeScript.actionIndex++; // IMPORTANT: don't save!
+        scriptSocket.processInput(socket_in_p1, Json.toJson(activeScript));
+        activeScript.actionIndex--;
+        scriptSocket.processInput(socket_in_p2, Json.toJson(activeScript));
+
+        verify(socket_out_p1, times(1)).write(Json.toJson(activeScript));
+        verify(socket_out_p2, times(1)).write(Json.toJson(activeScript));
+
+        // and the old version
+        activeScript.actionIndex++;
+        verify(socket_out_p1, times(1)).write(Json.toJson(activeScript));
+        verify(socket_out_p2, times(1)).write(Json.toJson(activeScript));
+    }
+
+    private ActiveScript activateScript() {
+        Script s = Script.find.byId(1L);
+        ActiveScript as = new ActiveScript();
+        as.actionIndex = 0;
+        as.runningTime = new Date().getTime();
+        as.script = s;
+        as.save();
+        return as;
+    }
+
+
 }
