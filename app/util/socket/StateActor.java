5ca0110ab3e29790aef0d52f10f7d708e033704f
Hidde Lycklama <mail.hidde@gmail.com>
2016-06-08 12:34:24 +0200

    Move socket actor functionality to separate classes Add no active script message on director view when no script is active Remove old main template Fix material ui elements

---
+++ app/util/socket/StateActor.java
@@ -0,0 +1,30 @@
+package util.socket;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import models.ActiveScript;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Saves running script state and communicates to all.
+ */
+public class StateActor extends SocketActor {
+
+    @Override
+    public boolean canAct(JsonNode jsonNode) {
+        return true;
+    }
+
+    @Override
+    public void act(JsonNode jsonNode) {
+        List<ActiveScript> aslist = ActiveScript.find.all();
+        if (!aslist.isEmpty()) { // we have an active script
+            Collections.sort(aslist.get(0).script.actions);
+            ActiveScript as = aslist.get(0);
+            as.actionIndex = jsonNode.findPath("actionIndex").asInt();
+            as.save(); // save it
+        }
+    }
+
+}
